eb62fff3eb911097edab9a53288f3eec428a79fd
JoakimSorensen <kimyoa0202@gmail.com>
2022-04-03 15:07:44 +0900

    completed basic chain, 3 blocks

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -4,3 +4,13 @@ Blockchain::Blockchain() {
 	_vChain.emplace_back(Block(0, "Genesis Block"));
 	_nDifficulty = 6;
 }
+
+void Blockchain::addBlock(Block bNew) {
+	bNew.sPrevHash = _GetLastBlock().getHash();
+	bNew.mineBlock(_nDifficulty);
+	_vChain.push_back(bNew);
+}
+
+Block Blockchain::_GetLastBlock() const {
+	return _vChain.back();
+}
--- BlockchainMain.cpp
+++ BlockchainMain.cpp
@@ -1,3 +1,17 @@
+#include "Blockchain.h"
+
 int main() {
-return 0;
+	Blockchain bChain = Blockchain();
+
+	std::cout << "Mining block 1..." << std::endl;
+	bChain.addBlock(Block(1, "Block 1 Data"));
+
+	
+	std::cout << "Mining block 2..." << std::endl;
+	bChain.addBlock(Block(2, "Block 2 Data"));
+
+	std::cout << "Mining block 3..." << std::endl;
+	bChain.addBlock(Block(3, "Block 3 Data"));
+
+	return 0;
 }
