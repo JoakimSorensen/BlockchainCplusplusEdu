65b2d441d5350ac617cebef7873b26fba3a3ac3d
JoakimSorensen <kimyoa0202@gmail.com>
2022-04-18 16:32:58 +0900

    added mock nodes

--- BlockchainMain.cpp
+++ BlockchainMain.cpp
@@ -1,4 +1,5 @@
 #include "Blockchain.h"
+#include <climits>
 
 inline const char* boolToString(bool b) {
 	return b ? "Chain validated" : "chain invalid";
@@ -7,8 +8,21 @@ inline const char* boolToString(bool b) {
 int main() {
 	Blockchain bChain = Blockchain();
 	bool chainIntegrity = true;
+	int numberOfNodes = 1;
 	
 	uint32_t indexIn = 1;
+
+	std::cout << "Enter number of nodes for the chain: " << std::endl;
+	std::cin >> numberOfNodes;
+	
+	Blockchain bNodes[numberOfNodes];
+
+	// Ignore to the end of line
+	std::cin.ignore(INT_MAX, '\n');
+	
+	std::cout << "Mining genesis block..." << std::endl;
+	bChain.addBlock(0, "Genesis");
+
 	while (true) {
 		char data[256];
 
@@ -19,8 +33,17 @@ int main() {
 
 		std::cout << "Mining block " << indexIn << "..." << std::endl;
 		chainIntegrity = bChain.addBlock(indexIn, strData);
-		
+
 		std::cout << boolToString(chainIntegrity) << std::endl;
+		
+		// distribute validated chain to nodes
+		// TODO: implement some conscensus
+		int i = 0;
+		while (chainIntegrity && i < numberOfNodes) {
+			bNodes[i] = bChain;
+			i++;
+		}
+		
 		indexIn++;
 	}
 	std::cout << "Shutting down chain..." << std::endl;
